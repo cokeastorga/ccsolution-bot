c281773d52615a237b8a2eb2de91e2725bc89169
Jorge Astorga <cokeastorgac@gmail.com>
2025-11-25 18:05:31 -0300

    Respuestas Hibridas

--- src/lib/chatbot/data/stores.ts
+++ src/lib/chatbot/data/stores.ts
@@ -10,16 +10,26 @@ export const stores: Store[] = [
   {
     id: 's1',
     nombre: 'Sucursal Av. Francia',
-    direccion: 'Av. Francia 1540, Valdivia'
+    direccion: 'Av. Francia 393, Valdivia'
   },
   {
     id: 's2',
-    nombre: 'Sucursal Isla Teja',
-    direccion: 'Los Robles 302, Isla Teja, Valdivia'
+    nombre: 'Sucursal Bueras',
+    direccion: 'Bueras 1822, Valdivia'
   },
   {
     id: 's3',
-    nombre: 'Sucursal Valle Volcanes',
-    direccion: 'Av. Cuarta Terraza 505, Valle Volcanes, Valdivia'
+    nombre: 'Sucursal Chacabuco',
+    direccion: 'Chacabuco 340, Valdivia'
+  },
+  {
+    id: 's4',
+    nombre: 'Sucursal Baquedano',
+    direccion: 'Baquedano 657, Valdivia'
+  },
+  {
+    id: 's5',
+    nombre: 'Sucursal Arauco',
+    direccion: 'Arauco 327, Valdivia'
   }
 ];
